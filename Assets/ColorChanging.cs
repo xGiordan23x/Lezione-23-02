211dfbee0019f0e451ac00a319ba5b6f344d92d4
Giordano23 <francigio23@gmail.com>
2023-02-28 15:00:38 +0100

    aggioranto cambia colore

--- Assets/ColorChanging.cs
+++ Assets/ColorChanging.cs
@@ -1,66 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class ColorChanging : MonoBehaviour
 {
-    public Color[] colors; 
-    public float cycleTime = 1.0f; 
+    public Color[] colors;
+   
     public float fadeTime = 0.5f;
     private int currentIndex = 0;
-    private float timeSinceLastCycle = 0.0f; 
-    private Color startColor; 
-    private Color endColor; 
+    
     private float timeSinceLastChange = 0.0f;
-    private bool isChangingColor = false; 
+    private Color startColor;
+    private Color endColor;
+
+    private bool isChangingColor = false;
 
     void Start()
-    {        
+    {
         GetComponent<Renderer>().material.color = colors[0];
     }
 
     void Update()
     {
-       
+
         if (!isChangingColor)
         {
-           
-            timeSinceLastCycle += Time.deltaTime;
-
-            
-            if (timeSinceLastCycle >= cycleTime)
+            startColor = GetComponent<Renderer>().material.color;
+            currentIndex++;
+            if (currentIndex >= colors.Length)
             {
-                
-                timeSinceLastCycle = 0.0f;
-
-                
-                startColor = GetComponent<Renderer>().material.color;
-                currentIndex++;
-                if (currentIndex >= colors.Length)
-                {
-                    currentIndex = 0;
-                }
-                endColor = colors[currentIndex];
-
-                
-                isChangingColor = true;
-                timeSinceLastChange = 0.0f;
+                currentIndex = 0;
             }
+            endColor = colors[currentIndex];
+
+            isChangingColor = true;
+            timeSinceLastChange= 0f;
+
+
         }
 
-       
+
         if (isChangingColor)
         {
-           
             timeSinceLastChange += Time.deltaTime;
-
-            
             float progress = Mathf.Clamp01(timeSinceLastChange / fadeTime);
 
-           
+
             GetComponent<Renderer>().material.color = Color.Lerp(startColor, endColor, progress);
 
-            
+
             if (progress >= 1.0f)
             {
                 isChangingColor = false;
